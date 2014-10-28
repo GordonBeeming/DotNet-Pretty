8d2e3a8849912abd614f95cc54b53b2af2885467
Gordon Beeming <gordon@beeming.co.za>
2014-10-28 07:59:53 +0200

    crating folder if it doesn't exist

--- DotNetPretty/tools/chocolateyInstall.ps1
+++ DotNetPretty/tools/chocolateyInstall.ps1
@@ -1,2 +1,7 @@
-﻿xcopy ..\..\lib\net45\*.* "$([System.Environment]::GetFolderPath("mydocuments"))\Visual Studio 2013\Visualizers" /Y
-xcopy ..\binDependencies\*.* "$([System.Environment]::GetFolderPath("mydocuments"))\Visual Studio 2013\Visualizers" /Y
+﻿[string]$copyTo = "$([System.Environment]::GetFolderPath("mydocuments"))\Visual Studio 2013\Visualizers"
+if (!(Test-Path -LiteralPath $copyTo))
+{
+    New-Item -Path $copyTo -ItemType directory
+}
+xcopy ..\..\lib\net45\*.* "$copyTo" /Y
+xcopy ..\binDependencies\*.* "$copyTo" /Y

a6756a5a5d7fd806bd7123a75f7d392ecd747ee3
Gordon Beeming <gordon@beeming.co.za>
2014-10-28 08:10:07 +0200

    changes to chocolatey install

--- DotNetPretty/tools/chocolateyInstall.ps1
+++ DotNetPretty/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
-﻿[string]$copyTo = "$([System.Environment]::GetFolderPath("mydocuments"))\Visual Studio 2013\Visualizers"
-[string]$mainAssemblyPath = "..\..\lib\net45\*.*"
-[string]$binDependenciesAssemblyPath = "..\binDependencies\*.*"
+﻿[string]$ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+[string]$copyTo = "$([System.Environment]::GetFolderPath("mydocuments"))\Visual Studio 2013\Visualizers"
+[string]$mainAssemblyPath = "$ScriptDir\..\..\lib\net45\*.*"
+[string]$binDependenciesAssemblyPath = "$ScriptDir\..\binDependencies\*.*"
 if (!(Test-Path -LiteralPath $copyTo))
 {
     New-Item -Path $copyTo -ItemType directory
 }
-xcopy "$mainAssemblyPath" "$copyTo" /Y
-xcopy "$binDependenciesAssemblyPath" "$copyTo" /Y
+Copy-Item -Path "$mainAssemblyPath" -Destination "$copyTo" -Force
+Copy-Item -Path "$binDependenciesAssemblyPath" -Destination "$copyTo" -Force

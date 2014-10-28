569deecb1d0bf2341e17868efbc1f54219796ba7
Gordon Beeming <gordon@beeming.co.za>
2014-10-28 07:40:09 +0200

    even more testing

--- DotNetPretty/tools/chocolateyInstall.ps1
+++ DotNetPretty/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-﻿[System.Diagnostics.Process]::Start("http://bing.com/?q=$pwd")
-Write-Content "N:\temp\info.txt" $pwd
+﻿$ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+[System.Diagnostics.Process]::Start("http://bing.com/?q=$ScriptDir")
